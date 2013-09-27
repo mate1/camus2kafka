2e3224103f8bd53892e7e83b26d1ff051524079c
Boris Fersing <boris.fersing@mate1inc.com>
2013-09-27 10:57:10 -0400

    - Run the job using the Hadoop ToolRunner. - Allow the user to pass the job class name from the command line - Support for Generic Options - Added a Callback feature for the jobs

---
+++ src/main/scala/com/mate1/camus2kafka/JobRunner.scala
@@ -0,0 +1,62 @@
+package com.mate1.camus2kafka
+
+import org.apache.hadoop.util.{Tool, ToolRunner}
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Boris Fersing
+ * Date: 9/27/13
+ * Time: 10:01 AM
+ */
+
+/**
+ * This object is the JobRunner with the main method that gets called from the command line
+ * The first argument must be the class name of the job to run. All other arguments are passed to the job.
+ *
+ * If the job extends the Callback trait, then the success or the error callback gets called when the job finishes
+ */
+object JobRunner {
+
+  def main(args:Array[String]): Unit = {
+
+    // The user must provide at least the class name of the job he wants to run
+    args.size match {
+      case 0 => println("Please provide the class name of the Job you want to run.")
+      case size => {
+        val classOption = try {
+          Some(Class.forName(args(0)))
+        } catch {
+          case e: ClassNotFoundException => {
+            println("The job class %s can't be found.".format(args(0)))
+            None
+          }
+          case e => {
+            e.printStackTrace()
+            None
+          }
+        }
+
+        classOption match {
+          case Some(classz) => {
+
+            // The first argument is the class name, don't pass it to the job
+            val jobArgs = args.drop(1)
+
+            // Get a new instance of the job
+            val tool = classz.newInstance().asInstanceOf[Tool]
+
+            // Run the job and save the exitCode for later
+            val exitCode = ToolRunner.run(tool, jobArgs)
+
+            // If the job extends the Callback trait then run the callback corresponding to the exit code
+            tool match {
+              case cb : Callback => if (exitCode == 0) cb.successCallback else cb.errorCallback
+              case _ => ()
+            }
+          }
+          case None => ()
+        }
+      }
+    }
+  }
+}
--- src/main/scala/com/mate1/camus2kafka/WordCountJob.scala
+++ src/main/scala/com/mate1/camus2kafka/WordCountJob.scala
@@ -1,6 +1,6 @@
 package com.mate1.camus2kafka
 
-import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.conf.Configured
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.IntWritable
 import org.apache.hadoop.io.Text
@@ -9,11 +9,13 @@ import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.mapreduce.Reducer
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
-import org.apache.hadoop.util.GenericOptionsParser
+import org.apache.hadoop.util.{Tool, GenericOptionsParser}
 import scala.collection.JavaConverters._
 
-// This class performs the map operation, translating raw input into the key-value
-// pairs we will feed into our reduce operation.
+/**
+ * This class performs the map operation, translating raw input into the key-value
+ * pairs we will feed into our reduce operation.
+ */
 class TokenizerMapper extends Mapper[Object,Text,Text,IntWritable] {
   val one = new IntWritable(1)
   val word = new Text
@@ -26,9 +28,11 @@ class TokenizerMapper extends Mapper[Object,Text,Text,IntWritable] {
   }
 }
 
-// This class performs the reduce operation, iterating over the key-value pairs
-// produced by our map operation to produce a result. In this case we just
-// calculate a simple total for each word seen.
+/**
+ * This class performs the reduce operation, iterating over the key-value pairs
+ * produced by our map operation to produce a result. In this case we just
+ * calculate a simple total for each word seen.
+ */
 class IntSumReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
   override def reduce(key:Text, values:java.lang.Iterable[IntWritable], context:Reducer[Text,IntWritable,Text,IntWritable]#Context) = {
     val sum = values.asScala.foldLeft(0) { (t,i) => t + i.get }
@@ -36,17 +40,26 @@ class IntSumReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
   }
 }
 
-// This class configures and runs the job with the map and reduce classes we've
-// specified above.
-object WordCountJob {
+/**
+ * The actual job class
+ */
+class  WordCountJob extends Configured with Tool with Callback {
+  def run(args: Array[String]): Int = {
 
-  def main(args:Array[String]):Int = {
-    val conf = new Configuration()
-    val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
-    if (otherArgs.length != 2) {
+    val conf = getConf
+
+    if (conf.getBoolean("printconf", false)){
+      conf.asScala.foreach(entry => println(entry.getKey+" : "+entry.getValue))
+      println("\n=============\nJob Custom Params:\n")
+      args.foreach(println _)
+      println("\n")
+    }
+
+    if (args.length != 2) {
       println("Usage: wordcount <in> <out>")
       return 2
     }
+
     val job = new Job(conf, "word count")
     job.setJarByClass(classOf[TokenizerMapper])
     job.setMapperClass(classOf[TokenizerMapper])
@@ -54,8 +67,10 @@ object WordCountJob {
     job.setReducerClass(classOf[IntSumReducer])
     job.setOutputKeyClass(classOf[Text])
     job.setOutputValueClass(classOf[IntWritable])
+
     FileInputFormat.addInputPath(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path((args(1))))
+
     if (job.waitForCompletion(true)) 0 else 1
   }
 }
